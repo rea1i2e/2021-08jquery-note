635b776dc86b40737d7c8b3d5f055a24c4e2a8f9
yoshiaki <afmaar128@gmail.com>
2021-07-22 08:19:12 +0900

    add basic-format & method.html

--- script.js
+++ script.js
@@ -1,7 +1,19 @@
 $(function() {
-  $('h1').fadeOut(3000);
+
+  // $('h1').fadeOut(3000);
+  $('#id-name').fadeIn('slow');
+
+  $('#appear').click(function(){
+    $('#click-fadeIn').fadeIn();
+  });
+
+  // クリックすると文字色が変わる
+  $('#text-color').click(function(){
+    $('#change-color').css('color','blue')
+  });
+
+  // クリックするとテキストが変わるtextメソッド
+  
+
 });
 
-// $(function() {
-//   $('h1').hide();
-// });
