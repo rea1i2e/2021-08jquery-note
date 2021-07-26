22647492f8a08c9899bf4c574838b80ecc310a97
yoshiaki <afmaar128@gmail.com>
2021-07-26 10:51:44 +0900

    add modal-hover.html

--- script.js
+++ script.js
@@ -13,7 +13,38 @@ $(function() {
   });
 
   // クリックするとテキストが変わるtextメソッド
+  $('#text-contents').click(function(){
+    $('#change-text').text('答え：text')
+  });
   
+  // クリックするとhtmlごとが変わるhtmlメソッド
+  $('#html-contents').click(function(){
+    $('#change-html').html('答え：<a href="https://developer.mozilla.org/ja/docs/Web/HTML/Element">html</a>')
+  });
+
+  // 予約モーダル
+  $('#reserve-btn').click(function(){
+    $('#modal-wrapper').fadeIn()
+  });
+  
+  // 予約モーダルを閉じる
+  $('#close').click(function(){
+    $('#modal-wrapper').fadeOut()
+  });
+
+  // ホバー時、featureの説明文を表示
+  $('.feature-hover').hover(
+    function() {
+      //.feature-descに、.desc-activeを追加
+      //$(this).find('.text-contents').addClass('text-active');(見本)
+      $(this).find('.feature-desc').addClass('desc-active');
+    },
+    function() {
+      //.feature-descから.desc-activeを削除
+      $(this).find('.feature-desc').removeClass('desc-active');
+
+    }
+  )
 
 });
 
