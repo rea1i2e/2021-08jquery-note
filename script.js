d2802561d608bcd9d884e2b4cfd6ff4461e60833
yoshiaki <afmaar128@gmail.com>
2021-07-30 10:37:01 +0900

    add slide in jquery-sample.html

--- script.js
+++ script.js
@@ -32,6 +32,7 @@ $(function() {
     $('#modal-wrapper').fadeOut()
   });
   
+  
   // ホバー時、featureの説明文を表示
   $('.feature-hover').hover(
     function() {
@@ -44,6 +45,97 @@ $(function() {
       $(this).find('.feature-desc').removeClass('desc-active');
     }
   );
+  
+
+
+  //スライド
+  
+  // .index-btnのクリックイベント
+  $('.index-btn').click(function() {
+    
+    //.activeを削除（初期設定は、1枚目のスライドに.activeがついている
+    $('.active').removeClass('active');
+    
+    //.active-index-btnを削除
+    $('.active-index-btn').removeClass('active-index-btn');
+    
+    //クリックされたボタンのインデックス番号を変数に代入
+    var clickedIndex = $('.index-btn').index($(this));
+    
+    //クリックされた要素に.activeを追加
+    $('.slide').eq(clickedIndex).addClass('active');
+    
+    // クリックされた.index-btnに.active-index-btnを追加
+    $('.index-btn').eq(clickedIndex).addClass('active-index-btn');
+    // $('.index-btn').addClass('active-index-btn');
+    
+    // 1. 変数slideIndexに「.active」要素のインデックス番号を代入
+    var slideIndex=$('.slide').index($('.active'));
+    
+    // 3. change-btn要素を表示してください
+    $('.change-btn').show();
+    
+    // 2. ifとelse ifを用いて、「.change-btn」の表示/非表示をおこなってください
+    //インデックス番号が０のとき、.prev-btnは非表示に
+    if(slideIndex==0) {
+      $('.prev-btn').hide();
+      
+      //インデックス番号が3のとき、.next-btnは非表示に
+    } else if(slideIndex==3) {
+      $('.next-btn').hide();
+    }
+  });
+  
+  // .change-btnのクリックイベント
+  $('.change-btn').click(function() {
+
+    //$displaySlideにクリックされた.activeのオブジェクトを代入
+    var $displaySlide = $('.active');
+    
+    //$displayIndexBtnにクリックされた.active-index-btnのオブジェクトを代入
+    var $displayIndexBtn = $('.active-index-btn');
+
+    //.activeを削除
+    $displaySlide.removeClass('active');
+
+    //.active-index-btnを削除
+    $displayIndexBtn.removeClass('active-index-btn');
+
+    //クリックされたのが.next-btnなら.activeを次の要素に追加し、そうでなければ前の要素に追加する
+    if ($(this).hasClass('next-btn')) {
+      $displaySlide.next().addClass('active');
+    } else {
+      $displaySlide.prev().addClass('active');
+    }
+
+    //クリックされたのが.next-btnなら.active-index-btnを次の要素に追加し、そうでなければ前の要素に追加する
+    if ($(this).hasClass('next-btn')) {
+      $displayIndexBtn.next().addClass('active-index-btn');
+    } else {
+      $displayIndexBtn.prev().addClass('active-index-btn');
+    }
+    
+  
+    // 「.index-btn」のクリックイベントと同様の処理
+    // 1. 変数slideIndexに「.active」要素のインデックス番号を代入
+    var slideIndex=$('.slide').index($('.active'));
+    
+    // 3. change-btn要素を表示してください
+    $('.change-btn').show();
+    
+    
+    // 2. ifとelse ifを用いて、「.change-btn」の表示/非表示
+   //インデックス番号が０のとき、.prev-btnは非表示に
+    if(slideIndex==0) {
+      $('.prev-btn').hide();
+      
+       //インデックス番号が3のとき、.next-btnは非表示に
+    } else if(slideIndex==3) {
+      $('.next-btn').hide();
+    }
+
+    
+  });
     
   // アコーディオン機能を使ったFAQ
   // .answerはデフォルトで非表示
