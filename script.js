cbcf1e432ad661cee7bc407836d1e237e5cbdb5c
yoshiaki <afmaar128@gmail.com>
2021-08-05 09:07:10 +0900

    add tab-menu.html

--- script.js
+++ script.js
@@ -161,6 +161,25 @@ $(function() {
       $(this).find('span').text('-');
     }
   });
+
+  // タブメニュー
+
+  // ①タブをクリックしたら発動
+  $('.tab li').click(function() {
+ 
+    // ②クリックされたタブの順番を変数に格納
+    var index = $('.tab li').index(this);
+ 
+    // ③クリック済みタブのデザインを設定したcssのクラスを一旦削除
+    $('.tab li').removeClass('active-tab');
+ 
+    // ④クリックされたタブにクリック済みデザインを適用する
+    $(this).addClass('active-tab');
+ 
+    // ⑤コンテンツを一旦非表示にし、クリックされた順番のコンテンツのみを表示
+    $('.area ul').removeClass('show').eq(index).addClass('show');
+ 
+  });
   
 
 
